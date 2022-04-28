782c6573ce427e80165c035672b45eaea53070ad
turbosuns7 <turbosuns7@gmail.com>
2022-04-28 12:25:31 +0300

    hw_28_04_22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project1';
+  private date = new Date();
+  private day = 60 * 60 * 24 * 1000;
+  title = this.date.toLocaleDateString();
+  date1 = new Date(Date.now() + this.day).toLocaleDateString();
+  date2 = new Date(Date.now() + 4 * this.day).toLocaleDateString();
+  date3 = new Date(Date.now() + 7 * this.day).toLocaleDateString();
+  date4 = new Date(Date.now() + 8 * this.day).toLocaleDateString();
 }
